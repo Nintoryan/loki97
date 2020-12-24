e4c51af8b49290e08241790fa59254e0b93821ae
Nintoryan <thehappystudio4@gmail.com>
2020-12-25 00:26:30 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace loki97
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.encode = new System.Windows.Forms.Button();
+            this.decode = new System.Windows.Forms.Button();
+            this.decodedText = new System.Windows.Forms.RichTextBox();
+            this.encodedText = new System.Windows.Forms.RichTextBox();
+            this.decodedBytes = new System.Windows.Forms.RichTextBox();
+            this.encodedBytes = new System.Windows.Forms.RichTextBox();
+            this.key = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // encode
+            // 
+            this.encode.Location = new System.Drawing.Point(12, 479);
+            this.encode.Name = "encode";
+            this.encode.Size = new System.Drawing.Size(249, 42);
+            this.encode.TabIndex = 0;
+            this.encode.Text = "Зашифровать";
+            this.encode.UseVisualStyleBackColor = true;
+            this.encode.Click += new System.EventHandler(this.encode_Click);
+            // 
+            // decode
+            // 
+            this.decode.Location = new System.Drawing.Point(539, 479);
+            this.decode.Name = "decode";
+            this.decode.Size = new System.Drawing.Size(249, 42);
+            this.decode.TabIndex = 1;
+            this.decode.Text = "Расшифровать";
+            this.decode.UseVisualStyleBackColor = true;
+            // 
+            // decodedText
+            // 
+            this.decodedText.Location = new System.Drawing.Point(0, 30);
+            this.decodedText.Name = "decodedText";
+            this.decodedText.Size = new System.Drawing.Size(360, 128);
+            this.decodedText.TabIndex = 2;
+            this.decodedText.Text = "Тестовые данные для зашифровки";
+            // 
+            // encodedText
+            // 
+            this.encodedText.Location = new System.Drawing.Point(431, 30);
+            this.encodedText.Name = "encodedText";
+            this.encodedText.Size = new System.Drawing.Size(357, 128);
+            this.encodedText.TabIndex = 3;
+            this.encodedText.Text = "";
+            // 
+            // decodedBytes
+            // 
+            this.decodedBytes.Location = new System.Drawing.Point(0, 229);
+            this.decodedBytes.Name = "decodedBytes";
+            this.decodedBytes.Size = new System.Drawing.Size(360, 129);
+            this.decodedBytes.TabIndex = 4;
+            this.decodedBytes.Text = "";
+            // 
+            // encodedBytes
+            // 
+            this.encodedBytes.Location = new System.Drawing.Point(428, 229);
+            this.encodedBytes.Name = "encodedBytes";
+            this.encodedBytes.Size = new System.Drawing.Size(360, 129);
+            this.encodedBytes.TabIndex = 5;
+            this.encodedBytes.Text = "";
+            // 
+            // key
+            // 
+            this.key.Location = new System.Drawing.Point(204, 425);
+            this.key.Name = "key";
+            this.key.Size = new System.Drawing.Size(360, 34);
+            this.key.TabIndex = 6;
+            this.key.Text = "123456789FACB";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(101, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Расшифрованный текст";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(101, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(172, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Расшифрованные байты";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(536, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(160, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Зашифрованный текст";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(536, 209);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(165, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Зашифрованные байты";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(371, 405);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Ключ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 533);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.key);
+            this.Controls.Add(this.encodedBytes);
+            this.Controls.Add(this.decodedBytes);
+            this.Controls.Add(this.encodedText);
+            this.Controls.Add(this.decodedText);
+            this.Controls.Add(this.decode);
+            this.Controls.Add(this.encode);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button encode;
+        private System.Windows.Forms.Button decode;
+        private System.Windows.Forms.RichTextBox decodedText;
+        private System.Windows.Forms.RichTextBox encodedText;
+        private System.Windows.Forms.RichTextBox decodedBytes;
+        private System.Windows.Forms.RichTextBox encodedBytes;
+        private System.Windows.Forms.RichTextBox key;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace loki97
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private ulong[] K1 = new ulong[49];
+        private ulong[] K2 = new ulong[49];
+        private ulong[] K3 = new ulong[49];
+        private ulong[] K4 = new ulong[49];
+        private ulong[] SK = new ulong[49];
+        private ulong[] R = new ulong[17];
+        private ulong[] L = new ulong[17];
+        private void encode_Click(object sender, EventArgs e)
+        {
+            int keylength = key.Text.Length;
+            if (keylength > 32 || keylength < 9)
+            {
+                string message = "Неправильная длинна ключа!";
+                string caption = "Ключ должен содержать от 16 до 32 знаков в шестнадцатиричной системе счисления!";
+                _ = MessageBox.Show(caption, message, MessageBoxButtons.OK);
+                return;
+            }
+            K1[0] = Convert.ToUInt64(key.Text.Substring(0, 8),16);
+            K2[0] = Convert.ToUInt64(key.Text.Substring(8, keylength-8>=8?8:keylength-8),16);
+            if (keylength < 17 && keylength > 8)
+            {
+                K3[0] = f_function(K1[0], K2[0]);
+                K4[0] = f_function(K2[0], K1[0]);
+            }else if(keylength < 25 || keylength > 16)
+            {
+                K3[0] = Convert.ToUInt64(key.Text.Substring(16, keylength - 16 >= 8 ? 8 : keylength - 16),16);
+                K4[0] = f_function(K2[0], K1[0]);
+            }
+            else
+            {
+                K3[0] = Convert.ToUInt64(key.Text.Substring(16, keylength - 16 >= 8 ? 8 : keylength - 16),16);
+                K4[0] = Convert.ToUInt64(key.Text.Substring(24, keylength - 24 >= 8 ? 8 : keylength - 24),16);
+            }
+            ResolveKeys();
+
+            decodedBytes.Text = "";
+            encodedBytes.Text = "";
+            encodedText.Text = "";
+
+            var DecodedText = decodedText.Text;
+            var decodedB = Encoding.UTF8.GetBytes(DecodedText);
+            foreach(var b in decodedB)
+            {
+                decodedBytes.Text +=" "+ Convert.ToString(b,2);
+            }
+            var blocks = new List<ulong>();
+            var codedblocks = new List<ulong>();
+            for (int i = 0; i < (int)Math.Ceiling(decodedB.Length / 16d); i++)
+            {
+                string blockL = "";
+                string blockR = "";
+                int counter = 0;
+                for(int j = i * 16; j < (i + 1) * 16; j++)
+                {
+                    byte currentbyte = (j < decodedB.Length) ? decodedB[j] : (byte)32;
+                    string current = Convert.ToString(currentbyte, 2);
+                    if (counter < 8)
+                    {
+                        for (int k = 0; k < 8 - current.Length; k++)
+                        {
+                            blockL += "0";
+                        }
+                        blockL += current;
+                    }
+                    else
+                    {
+                        for (int k = 0; k < 8 - current.Length; k++)
+                        {
+                            blockR += "0";
+                        }
+                        blockR += current;
+                    }
+                    counter++;
+                }
+                blocks.Add(Convert.ToUInt64(blockL,2));
+                blocks.Add(Convert.ToUInt64(blockR,2));
+            }
+            for(int g = 0; g < (int)Math.Truncate(blocks.Count / 2d); g+=2)
+            {
+                L[0] = blocks[g];
+                R[0] = blocks[g+1];
+                for (int i = 1; i < 17; i++)
+                {
+                    R[i] = L[i - 1] ^ f_function(R[i - 1] + SK[3 * i - 2], SK[3 * i - 1]);
+                    L[i] = R[i - 1] + SK[3 * i - 2] + SK[3 * i];
+                }
+                codedblocks.Add(L[16]);
+                codedblocks.Add(R[16]);
+            }
+            var encodedBytesList = new List<byte>();
+            foreach(var block in codedblocks)
+            {
+                var block_string = Convert.ToString((long)block, 2);
+                block_string = block_string.Insert(block_string.Length, Multiply("0", 64 - block_string.Length));
+                for (int d = 0; d < 8; d++)
+                {
+                    var byte_string = block_string.Substring(d * 8, 8);
+                    var byte_value = Convert.ToByte(byte_string,2);
+                    encodedBytes.Text += byte_string + " ";
+                    encodedBytesList.Add(byte_value);
+                }
+            }
+            encodedText.Text = Encoding.UTF8.GetString(encodedBytesList.ToArray());
+            
+        }
+
+        private void ResolveKeys()
+        {
+            for(ulong i = 1; i < 49; i++)
+            {
+                SK[i] = K4[i - 1] ^ g_function(K1[i - 1], K3[i - 1], K2[i - 1], i);
+                K1[i] = SK[i];
+                K2[i] = K1[i - 1];
+                K3[i] = K2[i - 1];
+                K4[i] = K3[i - 1];
+            }
+        }
+        private ulong g_function(ulong k1,ulong k3, ulong k2,ulong i)
+        {
+            return f_function(k1 + k3 + (0x9E3779B97F4A7C15 * i), k2);
+        }
+
+        private ulong KP(ulong A, ulong B)
+        {
+            var A_chars = Convert.ToString((long)A, 2).ToCharArray();
+            int previousSize = A_chars.Length;
+            Array.Resize(ref A_chars, 64);
+            for (int i = previousSize; i < A_chars.Length; i++)
+            {
+                A_chars[i] = '0';
+            }
+            var B_chars = Convert.ToString((long)B, 2).ToCharArray();
+            for(int i = 0; i < 32; i++)
+            {
+                var cheker = i < B_chars.Length?B_chars[i]:'0';
+                if (cheker=='1')
+                {
+                    A_chars[i] = A_chars[i] == '1' ? '0' : '1';
+                    A_chars[i+32] = A_chars[i+32] == '1' ? '0' : '1';
+                }
+            }
+            var A_s = new string(A_chars);
+            var B_s = new string(B_chars);
+            return Convert.ToUInt64(A_s, 2);
+        }
+
+        private string E(ulong x)
+        {
+            //[4-0|63-56|58-48|52-40|42-32|34-24|28-16|18-8|12-0]
+            var binaryArray = Convert.ToString((long)x, 2);
+            binaryArray = binaryArray.Insert(0, Multiply("0",64 - binaryArray.Length));
+            var newValue = new List<bool>();
+            for (int i = 4; i >= 0; i--)  newValue.Add(binaryArray[i]=='1');
+            for (int i = 63; i >= 56; i--) newValue.Add(binaryArray[i] == '1');
+            for (int i = 58; i >= 48; i--) newValue.Add(binaryArray[i] == '1');
+            for (int i = 52; i >= 40; i--) newValue.Add(binaryArray[i] == '1');
+            for (int i = 42; i >= 32; i--) newValue.Add(binaryArray[i] == '1');
+            for (int i = 34; i >= 24; i--) newValue.Add(binaryArray[i] == '1');
+            for (int i = 28; i >= 16; i--) newValue.Add(binaryArray[i] == '1');
+            for (int i = 18; i >= 8; i--) newValue.Add(binaryArray[i] == '1');
+            for (int i = 12; i >= 0; i--) newValue.Add(binaryArray[i] == '1');
+            var xs = "";
+            foreach(var b in newValue)
+            {
+                xs += b ? "1" : "0";
+            }
+            return xs;
+        }
+        private ulong Sa(string binaryValue)
+        {
+            ulong result=0;
+            result += S1(Convert.ToUInt64(binaryValue.Substring(0, 13),2));
+            result += S2(Convert.ToUInt64(binaryValue.Substring(13, 11),2));
+            result += S1(Convert.ToUInt64(binaryValue.Substring(24, 13),2));
+            result += S2(Convert.ToUInt64(binaryValue.Substring(37, 11),2));
+            result += S2(Convert.ToUInt64(binaryValue.Substring(48, 11),2));
+            result += S1(Convert.ToUInt64(binaryValue.Substring(59, 13),2));
+            result += S2(Convert.ToUInt64(binaryValue.Substring(72, 11),2));
+            result += S1(Convert.ToUInt64(binaryValue.Substring(83, 13),2));
+            return result;
+        }
+        private ulong Sb(string binaryV)
+        {
+            ulong result = 0;
+            var binaryValue = binaryV.Insert(0, Multiply("0", 96 - binaryV.Length));
+            result += S2(Convert.ToUInt64(binaryValue.Substring(0, 13),2));
+            result += S2(Convert.ToUInt64(binaryValue.Substring(13, 13),2));
+            result += S1(Convert.ToUInt64(binaryValue.Substring(26, 11),2));
+            result += S1(Convert.ToUInt64(binaryValue.Substring(37, 11),2));
+            result += S2(Convert.ToUInt64(binaryValue.Substring(48, 13),2));
+            result += S2(Convert.ToUInt64(binaryValue.Substring(61, 13),2));
+            result += S1(Convert.ToUInt64(binaryValue.Substring(74, 11),2));
+            result += S1(Convert.ToUInt64(binaryValue.Substring(85, 11),2));
+            return result;
+        }
+
+        private ulong S1(ulong x)
+        {
+            var a = x ^ 0x1FFF;
+            a = a * a * a;
+            a = (a % 0x2911) & 0xFF;
+            return a;
+        }
+        private ulong S2(ulong x)
+        {
+            var a = x ^ 0x7FF;
+            a = a * a * a;
+            a = (a % 0xAA7) & 0xFF;
+            return a;
+        }
+
+        private ulong f_function(ulong A, ulong B)
+        {
+            return Sb(Convert.ToString((long)P(Sa(E(KP(A, B)))), 2));
+        }
+
+        private ulong P(ulong x)
+        {
+            string v = Convert.ToString((long)x, 2);
+            v = v.Insert(0, Multiply("0", 64 - v.Length));
+            string result = "";
+            int[] a =
+            {56,48,40,32,24,16,08,00,57,49,41,33,25,17,09,01,
+             58,50,42,34,26,18,10,02,59,51,43,35,27,19,11,03,
+             60,52,44,36,28,20,12,04,61,53,45,37,29,21,13,05,
+             62,54,46,38,30,22,14,06,63,55,47,39,31,23,15,07};
+            for(int i = 63; i >= 0; i--)
+            {
+                result += v[a[i]];
+            }
+            return Convert.ToUInt64(result,2);
+        }
+        public static string Multiply(string source, int multiplier)
+        {
+            StringBuilder sb = new StringBuilder(multiplier * source.Length);
+            for (int i = 0; i < multiplier; i++)
+            {
+                sb.Append(source);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
